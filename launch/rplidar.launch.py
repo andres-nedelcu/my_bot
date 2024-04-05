067eabbde34e31ee5758f288b816e238d4695261
Robot <andresnedpin@gmail.com>
2024-04-04 23:07:01 -0400

    pulling

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,11 +1,11 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
- 
+
 def generate_launch_description():
- 
+
     return LaunchDescription([
- 
+
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
@@ -15,7 +15,7 @@ def generate_launch_description():
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard',
-                'serial_baudrate':="115200"
+                'serial_baudrate': 115200
             }]
         )
     ])
